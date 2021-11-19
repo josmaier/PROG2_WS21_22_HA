aa0448a1fc9bba8400085321b160dcd2dfd84f46
josch <joscha.maier@live.de>
2021-11-19 17:01:55 +0100

    Bruh I have no clue

--- src/StringStack.java
+++ src/StringStack.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class StringStack {
 
     public String[] Stack;
@@ -29,14 +31,14 @@ public class StringStack {
         } else {
             this.Stack[0] = element;
             this.fix++;
+            return true;
         }
         return false;
     }
 
     public String pop() {
-
+        String mem;
         for (int i = (this.Stack.length - 1); i >= 0; i--) {
-            String mem;
             if (this.Stack[i] != null) {
                 mem = this.Stack[i];
                 this.Stack[i] = null;
@@ -46,5 +48,46 @@ public class StringStack {
         return null;
     }
 
+    public String peek(){
+        for (int i = (this.Stack.length - 1); i >= 0; i--) {
+            if (this.Stack[i] != null) {
+                return this.Stack[i];
+            }
+        }
+        return null;
+    }
 
+    public int getSize(){
+        int size = 0;
+        for (int i = 0; i < this.Stack.length; i++) {
+            if (this.Stack[i] != null) {
+                size++;
+            }
+        }
+        return size;
+    }
+
+    public boolean isEmpty(){
+        boolean emptyness = true;
+        for (int i = (this.Stack.length - 1); i >= 0; i--) {
+            if (this.Stack[i] != null) {
+                emptyness = false;
+            }
+        }
+        return emptyness;
+    }
+
+    public boolean isFull(){
+        boolean fullness = false;
+            if (this.Stack[this.Stack.length - 1] != null) {
+                fullness = true;
+            }
+        return fullness;
+    }
+
+    public String toString(){
+        String mem = "The size of the stack is: " + this.Stack.length + " and it currently contains "
+                + this.getSize() + " elements." + Arrays.toString(this.Stack);
+        return mem;
+    }
 }
--- src/StringStackTest.java
+++ src/StringStackTest.java
@@ -4,10 +4,18 @@ public class StringStackTest {
 
 
         StringStack x = new StringStack(3);
+        System.out.println(x.isEmpty());
         System.out.println(x.push("Cheese"));
+        System.out.println(x.peek());
         System.out.println(x.push("Bread"));
+        System.out.println(x.peek());
         System.out.println(x.push("mayo"));
-        System.out.println(x.push("wata"));
+        System.out.println(x.isEmpty());
         System.out.println(x.pop());
+        System.out.println(x.peek());
+        System.out.println();
+        System.out.println(x.getSize());
+        System.out.println(x.isFull());
+        System.out.println(x.toString());
     }
 }
