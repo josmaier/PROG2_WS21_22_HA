381702ef0c862484edef32c33d14da8da4f05386
josch <joscha.maier@live.de>
2021-12-22 11:56:38 +0100

    yes

--- src/FirstFXML.java
+++ src/FirstFXML.java
@@ -7,6 +7,12 @@ import javafx.stage.Stage;
 
 public class FirstFXML extends Application {
     
+    /*
+    Es kann sein, das der Pfad für die JavaFX libraries in launch.json neu gesetzt werden müssen,
+    bzw Linie 18 ausgeschnitten und wieder eingefügt werden muss um die FXML datei zu finden
+    */
+
+
     @Override
     public void start(Stage primaryStage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("TODO.fxml"));
